5a56727886e56e34fb5b27a5fabc98976f484638
itcast <liguangqing92@126.com>
2016-11-01 15:00:33 +0800

    初始化源代码

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include "make_log.h"
+
+int main(int argc,char* argv[])
+{
+    LOG("111","22","liguangqing");
+    return 0;
+}
